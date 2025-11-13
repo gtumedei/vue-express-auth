cc8ae07ecbca0bac7971391cf03124cb62822910
gtumedei <giannitumedei@gmail.com>
2025-11-13 17:09:08 +0000

    Partially implement the auth controller to cut the boring parts

--- vuepost-without-auth/backend/src/controllers/auth-controller.ts
+++ vuepost-without-auth/backend/src/controllers/auth-controller.ts
@@ -1,10 +1,37 @@
 import { Request, Response } from "express"
+import { User } from "../utils/auth"
+import { connection } from "../utils/db"
 
 export const register = async (req: Request, res: Response) => {
+  // Estrae username e password dal body della richiesta
+  const { username, password } = req.body
+
+  // Inserisce l'utente nel database
+  await connection.execute("INSERT INTO users (username, password) VALUES (?, ?)", [
+    username,
+    password,
+  ])
+
+  // Estrae i dati del nuovo utente
+  const [results] = await connection.execute(
+    "SELECT id, username, role FROM users WHERE username=?",
+    [username]
+  )
+  const newUser = (results as User[])[0]
+
   res.json({ message: "Registrazione effettuata con successo" })
 }
 
 export const login = async (req: Request, res: Response) => {
+  // Estrae username e password dal body della richiesta
+  const { username, password } = req.body
+
+  // Esegue la query al database per ottenere i dati dell'utente in base allo username
+  const [results] = await connection.execute(
+    "SELECT id, username, password, role FROM users WHERE username=?",
+    [username]
+  )
+
   res.json({ message: "Login effettuato con successo" })
 }
 
